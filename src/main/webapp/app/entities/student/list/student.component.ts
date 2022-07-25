24ef19811dd4243fc9f25788d7505b09da8c1410
kosovic-drasko <kosovic.drasko@montefarm.co.me>
2022-07-25 18:38:46 +0200

    init

---
+++ src/main/webapp/app/entities/student/list/student.component.ts
@@ -0,0 +1,83 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HttpResponse } from '@angular/common/http';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+import { IStudent } from '../student.model';
+import { StudentService } from '../service/student.service';
+import { StudentDeleteDialogComponent } from '../delete/student-delete-dialog.component';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
+
+@Component({
+  selector: 'jhi-student',
+  templateUrl: './student.component.html',
+  styleUrls: ['./student.scss'],
+})
+export class StudentComponent implements OnInit {
+  students?: IStudent[];
+  isLoading = false;
+  student_age?: number[];
+  student_name?: any[];
+  ime?: string;
+  ukupno?: any;
+  public displayedColumns = ['id', 'name', 'age'];
+  public dataSource = new MatTableDataSource<IStudent>();
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  constructor(protected studentService: StudentService, protected modalService: NgbModal) {}
+  loadAllRxj(): void {
+    this.studentService.query1().subscribe({
+      next: res => {
+        this.students = res;
+        this.student_age = res.map(val => val.age! * 10);
+        this.students = res.filter(val => val.name === this.ime);
+        this.dataSource.data = res.filter(val => val.name === this.ime);
+        this.getTotalProcjenjena();
+        // this.ukupno = res.reduce((acc, productsdet) => acc + productsdet.age!, 0);
+        console.log('Studenti iz boota ukupno godina======>>  ', this.ukupno);
+        console.log('Studenti iz boota name je  ', this.students);
+        console.log('Uvecane godine ===========>  ', this.student_age);
+      },
+    });
+  }
+  getTotalProcjenjena(): any {
+    return (this.ukupno = this.dataSource.filteredData.map(t => t.age).reduce((acc, value) => acc! + value!, 0));
+    console.log('Uvecane godine ===========>  ', this.ukupno);
+  }
+  loadAll(): void {
+    this.isLoading = true;
+    this.studentService.query().subscribe({
+      next: (res: HttpResponse<IStudent[]>) => {
+        this.isLoading = false;
+        this.students = res.body ?? [];
+        this.dataSource.data = res.body ?? [];
+        this.getTotalProcjenjena();
+        console.log(' ===========>  ', this.students);
+      },
+      error: () => {
+        this.isLoading = false;
+      },
+    });
+  }
+
+  ngOnInit(): void {
+    this.loadAll();
+    console.log('Proba');
+  }
+
+  trackId(_index: number, item: IStudent): number {
+    return item.id!;
+  }
+
+  delete(student: IStudent): void {
+    const modalRef = this.modalService.open(StudentDeleteDialogComponent, { size: 'lg', backdrop: 'static' });
+    modalRef.componentInstance.student = student;
+    // unsubscribe not needed because closed completes on modal close
+    modalRef.closed.subscribe(reason => {
+      if (reason === 'deleted') {
+        this.loadAll();
+      }
+    });
+  }
+}
